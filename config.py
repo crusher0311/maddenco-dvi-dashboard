a9cd77feabfef028cfc990c2e1c8b0162a2e793c
Dhrumil Chauhan <69631048+developerDhrumilChauhan@users.noreply.github.com>
2025-08-11 18:43:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 # config.py
 
 # MySQL Database Configuration
-DB_HOST = "ncr1"
+DB_HOST = "ncr1.int3rnet.net"
 DB_PORT = 3306
 DB_USER = "elitein1_maddencodvi"
 DB_PASSWORD = "hellotimepaass"
 DB_NAME = "elitein1_maddencodvi"
 DB_SSL = False
+
 
 
 
