45ae49f85f102b0a372a714035574759db239915
Dhrumil Chauhan <69631048+developerDhrumilChauhan@users.noreply.github.com>
2025-08-11 18:34:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # config.py
 
 # MySQL Database Configuration
-DB_HOST = "127.0.0.1"
+DB_HOST = "ncr1.int3rnet.net"
 DB_PORT = 3306
 DB_USER = "elitein1_maddencodvi"
 DB_PASSWORD = "hellotimepaass"
 DB_NAME = "elitein1_maddencodvi"
 ssl={"ssl": {}}
 DB_SSL = True  
+
 
 
 
